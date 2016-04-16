5d7b8b7ce8c5d61fd3fd81a0bc8800d593772477
pnookala <nookala.poornima@gmail.com>
2016-04-16 16:52:49 -0500

    Added code to save mean change values in a csv file.

--- code/project_main.py
+++ code/project_main.py
@@ -20,23 +20,23 @@ def main(argv):
     impute_data = True
 
     # Default options for debugging, use the command-line parameters to override
-    filePath = 'data/iris.data'
-    class_column = None
+    # filePath = 'data/iris.data'
+    # class_column = None
 
     # filePath = 'data/soybean-large.data'
     # class_column = 0
 
     # filePath = 'data/echocardiogram_clean.data'
-    # filePath = 'data/echocardiogram.data'
-    # class_column = 1
-    # ignored_columns = [0, 10]
+    filePath = 'data/echocardiogram.data'
+    class_column = 1
+    ignored_columns = [0, 10]
 
     delimiter = ','
     has_header = False
-    row_random_rate = [.7]
-    col_random_rate = [2]
-    # row_random_rate = [0]
-    # col_random_rate = [0]
+    # row_random_rate = [.7]
+    # col_random_rate = [2]
+    row_random_rate = [0]
+    col_random_rate = [0]
     # row_random_rate = np.arange(0, 1.0001, 0.1)
     # col_random_rate = np.arange(0, 8, 1)
     r_seed = 0
@@ -201,16 +201,36 @@ def process_data(data_source, _output, class_indices, classes, filePath, row_ran
     perform_classification(_data, classes, class_indices, parameters, statistics, row_random_rate, col_random_rate)
 
     if impute_data:
-        # Plot mean change in values
-        plt.plot(meanChangeInValues, "-", linewidth=2.0)
-        plt.xlabel('Iterations', fontsize=12)
-        plt.ylabel('Mean change in values', fontsize=12)
-        # plt.title("Mean Change in Values")
         fileName = ntpath.basename(filePath)
-        plt.savefig('output/MeanChangePlot_' + fileName + '.png')
-        plt.close()
+        plot_save_meanvalues(fileName, meanChangeInValues)
 
-        print('', end='', flush=True)
+
+def plot_save_meanvalues(fileName, meanChangeInValues):
+    # Plot mean change in values
+    plt.plot(meanChangeInValues, "-", linewidth=2.0)
+    plt.xlabel('Iterations', fontsize=12)
+    plt.ylabel('Mean change in values', fontsize=12)
+    # plt.title("Mean Change in Values")
+    plt.savefig('output/MeanChangePlot_' + fileName + '.png')
+    plt.close()
+
+    print('', end='', flush=True)
+
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
+
+    output_path = os.path.join(outputDir, "meanchangeresults.csv")
+    new_file = not os.path.exists(output_path)
+
+    headers = ("FileName", "Iteration", "Mean Change")
+    with open(output_path, 'a') as f:
+        if new_file:
+            f.write(','.join(itertools.chain(headers)))
+            f.write('\n')
+        for k in range(len(meanChangeInValues)):
+            f.write(','.join(str(x) for x in itertools.chain((fileName, k, meanChangeInValues[k]))))
+            f.write('\n')
+        f.flush()
 
 
 def perform_classification(data, classes, class_indices, parameters, statistics, row_random_rate, col_random_rate):
